6d71b1821b6d617d7a30de6f4e9140b3a45878b2
estanis06 <estanislau.giralt@estudiantat.upc.edu>
2025-01-14 11:28:00 +0100

    afegides gràfiques

--- ap/iperf_server.py
+++ ap/iperf_server.py
@@ -1,4 +1,6 @@
 import iperf3
+import matplotlib.pyplot as plt
+import os
 
 def run_iperf3_server():
     """
@@ -18,9 +20,52 @@ def run_iperf3_server():
             if result.error:
                 print(f"Error en el servidor: {result.error}")
             else:
-                print(f"Conexión desde: {result.remote_host}")
-                print(f"Throughput: {result.sent_Mbps:.2f} Mbps (enviado)")
-                print(f"Throughput: {result.received_Mbps:.2f} Mbps (recibido)")
+                
+                client_ip=result.remote_host
+                intervals = result.intervals
+                times = [interval['sum']['start'] for interval in intervals]
+                client_folder = f"reports/{client_ip}"
+                os.makedirs(client_folder, exist_ok=True)
+                
+                throughputs = [interval['sum']['bits_per_second'] / 1e6 for interval in intervals] #MBPS
+                packet_loss = [interval['sum']['lost_percent'] for interval in intervals]  # %
+                delays = [interval['sum']['jitter_ms'] for interval in intervals]  # ms (en UDP)
+
+                #Graphic 1
+                plt.figure(figsize=(10, 5))
+                plt.plot(times, throughputs, marker='o', label='Throughput (Mbps)')
+                plt.xlabel('Time (s)')
+                plt.ylabel('Throughput (Mbps)')
+                plt.title(f"Throughput of {client_ip}")
+                plt.legend()
+                plt.grid(True)
+                plt.savefig(f"{client_folder}/throughput.png") 
+                plt.close()
+
+
+                #Graphic 2
+                plt.figure(figsize=(10, 5))
+                plt.plot(times, packet_loss, marker='o', label='Packet loss (%)')
+                plt.xlabel('Time (s)')
+                plt.ylabel('Packet loss (%)')
+                plt.title(f"Packet loss of {client_ip}")
+                plt.legend()
+                plt.grid(True)
+                plt.savefig(f"{client_folder}/packet_loss.png") 
+                plt.close()
+
+
+                #Graphic 3
+                plt.figure(figsize=(10, 5))
+                plt.plot(times, delays, marker='o', label='Jitter (ms)')
+                plt.xlabel('Time (s)')
+                plt.ylabel('Jitter (ms)')
+                plt.title(f"Delay of {client_ip}")
+                plt.legend()
+                plt.grid(True)
+                plt.savefig(f"{client_folder}/delay.png") 
+                plt.close()
+
     except KeyboardInterrupt:
         print("\nServer stopped.")
     except Exception as e:
